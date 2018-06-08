6098dc873fa6fdf2fdbd1ee7af4898acc6fea473
Emilio Ferreiras <emilio.rojas05@gmail.com>
2018-06-08 17:08:29 -0400

    creacion del proyecto, inico de encapsulamiento

---
+++ Practica3/src/encapsulacion/Articulo.java
@@ -0,0 +1,14 @@
+
+import java.io.Serializable;
+import package.Usuario;
+
+public class Articulo implements Serializable{
+
+    private long id;
+    private String titulo;
+    private String cuerpo;
+    private Usuario autor;
+    private Date fecha;
+    private list
+
+}
