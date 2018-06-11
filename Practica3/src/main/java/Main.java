465c5608f2f4f3860590748c4b4371ff6461e932
Emilio Ferreiras <emilio.rojas05@gmail.com>
2018-06-11 08:57:23 -0400

    usuario por defecto

--- Practica3/src/main/java/Main.java
+++ Practica3/src/main/java/Main.java
@@ -1,8 +1,13 @@
 
+import dao.Sql2oUsuarioDao;
+import encapsulacion.Usuario;
+import freemarker.template.Configuration;
 import servicios.*;
 import java.sql.SQLException;
 import java.util.List;
 
+import Rutas.RutasWeb;
+import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
 
@@ -13,5 +18,13 @@ public class Main {
         DBService.getInstancia().testConexion();
 
         ConnectionService.stopDb();
-    }
+
+        Configuration configuration=new Configuration(Configuration.VERSION_2_3_23);
+        configuration.setClassForTemplateLoading(Main.class, "/");
+
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(configuration);
+
+
+        new RutasWeb(freeMarkerEngine);
+        }
 }
