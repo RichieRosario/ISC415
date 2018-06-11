9291d4fc595f248c9d9facb3aae01eab380f1ba8
Ricardo Rosario <20110714@ce.pucmm.edu.do>
2018-06-11 08:47:03 -0400

    Templates
    agregando templates

--- Practica3/src/main/java/Main.java
+++ Practica3/src/main/java/Main.java
@@ -1,17 +1,62 @@
 
+import encapsulacion.Articulo;
+import encapsulacion.Usuario;
 import servicios.*;
+
+import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.Version;
+import spark.Spark;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        ConnectionService.startDb();
+       // ConnectionService.startDb();
+        final Configuration configuration = new Configuration(new Version(2, 3, 0));
+        configuration.setClassForTemplateLoading(Main.class, "/");
+        Spark.staticFileLocation("/public/");
+        Spark.get("/", (request, response) -> {
+        List<Articulo> articulos = new ArrayList<Articulo>();
 
-        DBService.getInstancia().testConexion();
+         Articulo p = new Articulo(1,);
+        p.setTitulo("PRobando");
+        p.setCuerpo("ola k ase");
+        p.setFecha(Date.valueOf("20-05-2018"));
+        articulos.add(p);
+            StringWriter writer = new StringWriter();
+            try {
+                Template formTemplate = configuration.getTemplate("templates/index.ftl");
+                Map<String, Object> map = new HashMap<>();
+                map.put("articulos",articulos);
 
-        ConnectionService.stopDb();
+
+                formTemplate.process(map, writer);
+            } catch (Exception e) {
+                Spark.halt(500);
+            }
+
+
+
+
+
+
+            return writer;
+        });
+      //  DBService.getInstancia().testConexion();
+       // ConnectionService.crearTablas();
+       // ConnectionService.stopDb();
     }
 }
--- Practica3/src/main/java/Rutas/RutasWeb.java
+++ Practica3/src/main/java/Rutas/RutasWeb.java
@@ -1,9 +1,9 @@
 package Rutas;
 
+
 import com.google.gson.Gson;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
-
 import java.util.HashMap;
 import java.util.Map;
 
